9b8cf3a1cf4c5995cce53b98d1f0559362ba4cff
helenypan <yue.pan@me.com>
2017-06-12 22:55:33 +0100

    modify the main script in hello-world

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -14,13 +14,30 @@
 
 import webapp2
 
+form = '''
+<form method="POST" action="/testform">
+    <input name="q">
+    <input type ="submit">
+</form>
+'''
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        #default content type is text/html
+        # self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(form)
 
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        q = self.request.get("q")
+        # self.response.out.write(q)
+
+        # self.response.headers['Content-Type'] = 'text/html'
+        # self.response.out.write(form)
+
+        self.response.headers['Content-Type'] = "text/plain"
+        self.response.out.write(self.request)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage),('/testform',TestHandler)
 ], debug=True)
